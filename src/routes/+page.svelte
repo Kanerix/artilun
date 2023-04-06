a6b40da976956b2f1347595d5a6323e78cf0a8a1
Kanerix <dkkasjoe@hotmail.com>
2023-04-06 23:27:23 +0200

    Worked on frontpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,30 @@
-<a href="/dash/general/home">
-	Dashboard
-</a>
+<script>
+
+    import Paper from '../components/Paper.svelte';
+    import PaperBody from '../components/PaperBody.svelte';
+    import PaperHeader from '../components/PaperHeader.svelte';
+
+</script>
+
+<div class="flex w-full items-center justify-evenly">
+	<a href="/register">
+		<Paper>
+			<PaperHeader
+				header="Register"
+			/>
+			<PaperBody>
+				Don't have an account?
+			</PaperBody>
+		</Paper>
+	</a>
+	<a href="/login">
+		<Paper>
+			<PaperHeader
+				header="Login"
+			/>
+			<PaperBody>
+				Already have an account?
+			</PaperBody>
+		</Paper>
+	</a>
+</div>
